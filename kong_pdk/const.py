e0ff37a381a6fe2c8e963ee17cc4ae84dabb18bc
Wangchong Zhou <fffonion@gmail.com>
2023-06-20 16:11:33 +0800

    release: 0.3.5 (#123)

--- kong_pdk/const.py
+++ kong_pdk/const.py
@@ -13,7 +13,7 @@ EXEBUNDLE = getattr(sys, 'frozen', False)
 LOCALE = locale.getdefaultlocale()[0]
 CODEPAGE = locale.getdefaultlocale()[1] or 'ascii'
 
-__version__ = 0.34
+__version__ = 0.35
 
 # https://github.com/soimort/you-get/you-get
 if getattr(sys, 'frozen', False):
